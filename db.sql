88c4197a7ad39f0b24c319f0c715d978d956725f
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-15 11:44:59 +0530

    Update db.sql

---
+++ db.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 15, 2018 at 06:14 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `commented_by` varchar(60) NOT NULL,
+  `body` tinytext NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT '',
+  `commented_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_closed` tinyint(1) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `post_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `comment_id` (`comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `commented_by`, `body`, `image`, `commented_time`, `user_closed`, `deleted`, `post_id`) VALUES
+(1, 'jitendra', 'hi', '', '2018-10-12 18:21:11', 0, 0, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment_likes`
+--
+
+DROP TABLE IF EXISTS `comment_likes`;
+CREATE TABLE IF NOT EXISTS `comment_likes` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) NOT NULL,
+  `liked` tinyint(4) NOT NULL DEFAULT '0',
+  `comment_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friend_requests`
+--
+
+DROP TABLE IF EXISTS `friend_requests`;
+CREATE TABLE IF NOT EXISTS `friend_requests` (
+  `request_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_from` varchar(60) NOT NULL,
+  `user_to` varchar(60) NOT NULL,
+  `request_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `viewed` tinyint(1) NOT NULL DEFAULT '0',
+  `accepted` tinyint(1) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`request_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `message_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_from` varchar(60) NOT NULL,
+  `user_to` varchar(60) NOT NULL,
+  `message_body` text NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `viewed` tinyint(1) NOT NULL DEFAULT '0',
+  `opened` tinyint(1) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`message_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+DROP TABLE IF EXISTS `notifications`;
+CREATE TABLE IF NOT EXISTS `notifications` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_from` varchar(60) NOT NULL,
+  `user_to` varchar(60) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `notification_body` tinytext NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `notification_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `viewed` tinyint(1) NOT NULL DEFAULT '0',
+  `opened` tinyint(1) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `post_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_from` varchar(60) NOT NULL,
+  `user_to` varchar(60) NOT NULL,
+  `body` text NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT '',
+  `posted_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_closed` tinyint(1) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`post_id`),
+  KEY `post_id` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_likes`
+--
+
+DROP TABLE IF EXISTS `post_likes`;
+CREATE TABLE IF NOT EXISTS `post_likes` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) NOT NULL,
+  `liked` tinyint(4) NOT NULL DEFAULT '1',
+  `post_id` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `signup_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `profile_pic` varchar(255) NOT NULL,
+  `deactivate_account` tinyint(1) NOT NULL DEFAULT '0',
+  `friend_array` text,
+  `moblie` varchar(15) DEFAULT NULL,
+  `gender` varchar(6) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `home_town` tinytext,
+  `city` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `school` text,
+  `college` text,
+  `position` text,
+  `company` text,
+  `bio` text,
+  `is_online` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login_durations`
+--
+
+DROP TABLE IF EXISTS `user_login_durations`;
+CREATE TABLE IF NOT EXISTS `user_login_durations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) NOT NULL,
+  `login_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `logout_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
