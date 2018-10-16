3aa8d93b1bb8a141badca971b4a3893ca7118b9f
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-17 00:08:11 +0530

    Update infinite_post_loading.php

--- infinite_post_loading.php
+++ infinite_post_loading.php
@@ -16,20 +16,23 @@
   </div>
   <div class="post">
     <div class="card shadow p-3 bg-white rounded">
-      <form>
-        <div class="form-group">
-          <textarea class="form-control border border-primary" rows="5" maxlength="60000" id="post" placeholder="Post Something here..." style="margin-bottom: 10px;"></textarea>
-          <div class="form-row">
-            <div class="col">
-              <input type="file" class="form-control-file btn float-left" style="padding-left: 0px;">
-            </div>
-            <div class="col">
-              <a href="#" class="btn btn-primary float-right"><i class="fa fa-pencil"></i>&nbsp;Post</a>
-            </div>
-          </div>
+      <div class="form-group">
+        <textarea class="form-control border border-primary" rows="5" maxlength="60000" id="post" placeholder="Post Something here..." style="margin-bottom: 10px;"></textarea>
+      </div>
+      <div class="form-row">
+        <div class="col">
+          <p class="text-muted float-left">Like()</p>
+          <p class="text-muted float-right">Comments()</p>
         </div>
-      </form>
+      </div>
+      <div class="form-row">
+        <div class="col">
+          <button class="btn btn-primary float-left"><i class="fa fa-thumbs-o-up"></i>&nbsp;Like</button>
+          <button class="btn btn-primary float-left"><i class="fa fa-comment-o"></i>&nbsp;Comment</button>
+        </div>
+      </div>
     </div>
+    
     <div class="card shadow p-3 bg-white rounded">
       <form>
         <div class="form-group">
