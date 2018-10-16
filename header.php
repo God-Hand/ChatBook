93a236eb168ef240f040f22f511fc4f12dd3f775
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-16 23:12:42 +0530

    header.php updated

--- header.php
+++ header.php
@@ -12,6 +12,11 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<style type="text/css">
+  .form-group, .table{
+    margin-bottom: 0px;
+  }
+</style>
 
 <nav class="mb-4 navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
   <a class="navbar-brand" href="index.php">ChatBook</a>
