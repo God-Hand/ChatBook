31b501fb942852cb2a9f79e6a419cd538152df73
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-16 19:01:00 +0530

    Update profile_card.php

--- profile_card.php
+++ profile_card.php
@@ -1,13 +1,13 @@
 <style type="text/css">
 
 	/*Profile card*/
-	.profile-card .card-img-block{
+	.profile-card .profile-card-img-block{
 	    float:left;
 	    width:100%;
 	    height:150px;
 	    overflow:hidden;
 	}
-	.profile-card .card-body{
+	.profile-card .profile-card-body{
 	    position:relative;
 	}
 	.profile-card .profile {
@@ -18,10 +18,10 @@
 	    width:100px;
 	    margin-left: -50px;
 	}
-	.profile-card .card-img-block{
+	.profile-card .profile-card-img-block{
 	    position:relative;
 	}
-	.profile-card .card-img-block > .info-box{
+	.profile-card .profile-card-img-block > .profile-info-box{
 	    position:absolute;
 	    width:100%;
 	    height:100%;
@@ -33,7 +33,7 @@
 	    transition: 1s ease;
 	    opacity:0;
 	}
-	.profile-card .card-img-block:hover > .info-box{
+	.profile-card .profile-card-img-block:hover > .profile-info-box{
 	    opacity:1;
 	    -webkit-transition: all 1s ease;
 	    transition: all 1s ease;
@@ -42,11 +42,13 @@
 
 <div class="col-md-4">
 	<div class="card profile-card shadow-lg p-3 mb-5 bg-white rounded">
-    <div class="card-img-block">
-      <div class="info-box bg-primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+    <div class="profile-card-img-block">
+      <div class="profile-info-box bg-primary">
+      	info about user
+      </div>
       <img src="https://images.pexels.com/photos/965157/pexels-photo-965157.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" >
     </div>
-    <div class="card-body pt-5">
+    <div class="profile-card-body pt-5">
       <img src="https://randomuser.me/api/portraits/women/14.jpg" alt="profile-image" class="profile border border-primary"/>
       <h5>Gail Schmidt</h5>
       <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
