597411166a1787c79a12232ffe2f0f0d7c087843
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-17 00:10:36 +0530

    Update infinite_post_loading.php

--- infinite_post_loading.php
+++ infinite_post_loading.php
@@ -32,21 +32,5 @@
         </div>
       </div>
     </div>
-    
-    <div class="card shadow p-3 bg-white rounded">
-      <form>
-        <div class="form-group">
-          <textarea class="form-control border border-primary" rows="5" maxlength="60000" id="post" placeholder="Post Something here..." style="margin-bottom: 10px;"></textarea>
-          <div class="form-row">
-            <div class="col">
-              <input type="file" class="form-control-file btn float-left" style="padding-left: 0px;">
-            </div>
-            <div class="col">
-              <a href="#" class="btn btn-primary float-right"><i class="fa fa-pencil"></i>&nbsp;Post</a>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
   </div>
 </div>
