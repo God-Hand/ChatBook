d92bb58691fa92bab784bacd6016765801a547ad
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-21 10:33:38 +0530

    post_body completed

--- post_body_example.php
+++ post_body_example.php
@@ -15,7 +15,7 @@
     </form>
   </div>
   <div class="post">
-    <div class="card shadow p-3 mb-2 bg-white rounded">
+    <div class="card shadow p-3 bg-white rounded">
       <div class="media p-3">
         <img src="https://randomuser.me/api/portraits/women/14.jpg" alt="username" class="align-self-start mr-3 rounded-circle" style="width:60px;">
         <div class="media-body">
@@ -36,7 +36,30 @@
           <button class="btn btn-primary float-left"><i class="fa fa-comment-o"></i>&nbsp;Comment</button>
         </div>
       </div>
-      <iframe src="infinite_comment.php"></iframe>
+    </div>
+  </div>
+  <div class="post">
+    <div class="card shadow p-3 bg-white rounded">
+      <div class="media p-3">
+        <img src="https://randomuser.me/api/portraits/women/14.jpg" alt="username" class="align-self-start mr-3 rounded-circle" style="width:60px;">
+        <div class="media-body">
+          <h5>John Doe<h6><small><i>Posted on February 19, 2016</i></small></h5>
+        </div>
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <img src="https://randomuser.me/api/portraits/women/14.jpg" alt="username" style="width: 100%; height: 100%;">
+      <div class="form-row">
+        <div class="col">
+          <p class="text-muted float-left">Like()</p>
+          <p class="text-muted float-right">Comments()</p>
+        </div>
+      </div>
+      <div class="form-row">
+        <div class="col">
+          <button class="btn btn-primary float-left" style="margin-right:2px;"><i class="fa fa-thumbs-o-up"></i>&nbsp;Like</button>
+          <button class="btn btn-primary float-left"><i class="fa fa-comment-o"></i>&nbsp;Comment</button>
+        </div>
+      </div>
     </div>
   </div>
 </div>
