50374d11dd9fc7972a848192d4f31def4f01f930
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-05 20:43:33 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -11,8 +11,7 @@
 				<div class="list-group ">
 				  <div class="nav nav-tabs" id="nav-tab" role="tablist">
 				    <a class="list-group-item list-group-item-action active" id="accountInfo-item" data-toggle="tab" href="#accountInfo" role="tab" aria-controls="accountInfo" aria-selected="true">Edit Account Info</a>
-				    <a class="list-group-item list-group-item-action" id="personalInfo-item" data-toggle="tab" href="#personalInfo" role="tab" aria-controls="personalInfo" aria-selected="false">Add/Edit Personal Info</a>
-				    <a class="list-group-item list-group-item-action" id="educationInfo-item" data-toggle="tab" href="#educationInfo" role="tab" aria-controls="educationInfo" aria-selected="false">Add/Edit Education Info</a>
+				    <a class="list-group-item list-group-item-action" id="personalInfo-item" data-toggle="tab" href="#personalInfo" role="tab" aria-controls="personalInfo" aria-selected="false">Edit Personal Info</a>
 				    <a class="list-group-item list-group-item-action" id="uploadPics-item" data-toggle="tab" href="#uploadPics" role="tab" aria-controls="uploadPics" aria-selected="false">Edit Images</a>
 				    <a class="list-group-item list-group-item-action" id="changePassword-item" data-toggle="tab" href="#changePassword" role="tab" aria-controls="changePassword" aria-selected="false">Change Password</a>
 				    <a class="list-group-item list-group-item-action" id="deleteAccount-item" data-toggle="tab" href="#deleteAccount" role="tab" aria-controls="deleteAccount" aria-selected="false">Delete Account</a>
@@ -38,21 +37,57 @@
 					<div class="row">
 			      <div class="card-body">
 							<div class="tab-content" id="nav-tabContent">
-							  <div class="tab-pane fade show active" id="accountInfo" role="tabpanel" aria-labelledby="nav-home-tab">accountInfo</div>
-							  <div class="tab-pane fade" id="personalInfo" role="tabpanel" aria-labelledby="nav-profile-tab">personalInfo</div>
-							  <div class="tab-pane fade" id="educationInfo" role="tabpanel" aria-labelledby="nav-contact-tab">educationInfo</div>
-							  <div class="tab-pane fade" id="uploadPics" role="tabpanel" aria-labelledby="nav-profile-tab">uploadPics</div>
-							  
-							  <div class="tab-pane fade" id="changePassword" role="tabpanel" aria-labelledby="nav-home-tab">
+
+							  <div class="tab-pane fade show active" id="accountInfo" role="tabpanel" aria-labelledby="nav-home-tab">
 							  	<div class="row">
 		                <div class="col-md-12">
-		                  <h4>Change Password</h4>
+		                  <h4>Edit Account Info</h4>
+		                  <hr>
+		                  <form>
+                        <div class="form-group row">
+                          <label for="firstName" class="col-4 col-form-label">First Name</label> 
+                          <div class="col-8">
+                            <input id="firstName" name="firstName" placeholder="First Name" class="form-control here" type="text">
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <label for="lastName" class="col-4 col-form-label">Last Name</label> 
+                          <div class="col-8">
+                            <input id="lastName" name="lastName" placeholder="Last Name" class="form-control here" type="text">
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <label for="email" class="col-4 col-form-label">Email</label> 
+                          <div class="col-8">
+                            <input id="email" name="email" placeholder="Email" class="form-control here" type="email">
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <label for="bio" class="col-4 col-form-label">Bio</label> 
+                          <div class="col-8">
+                            <textarea id="bio" name="bio" maxlength="255" cols="40" rows="4" class="form-control" placeholder="Write about you."></textarea>
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <div class="offset-4 col-8">
+                            <button name="savAccountInfo" type="submit" class="btn btn-primary">Save</button>
+                          </div>
+                        </div>
+                      </form>
+		                </div>
+			            </div>
+							  </div>
+
+							  <div class="tab-pane fade" id="personalInfo" role="tabpanel" aria-labelledby="nav-contact-tab">
+							  	<div class="row">
+		                <div class="col-md-12">
+		                  <h4>Edit Personal Information</h4>
 		                  <hr>
 		                  <form>
                         <div class="form-group row">
                           <label for="username" class="col-4 col-form-label">Password</label> 
                           <div class="col-8">
-                            <input id="username" name="username" placeholder="Password" class="form-control here" required="required" type="text">
+                            <input id="username" name="username" placeholder="Password" class="form-control here" type="text">
                           </div>
                         </div>
                         <div class="form-group row">
@@ -70,6 +105,42 @@
                         <div class="form-group row">
                           <div class="offset-4 col-8">
                             <button name="submit" type="submit" class="btn btn-primary">Save</button>
+                          </div>
+                        </div>
+                      </form>
+		                </div>
+			            </div>
+							  </div>
+							  
+							  <div class="tab-pane fade" id="uploadPics" role="tabpanel" aria-labelledby="nav-profile-tab">uploadPics</div>
+							  
+							  <div class="tab-pane fade" id="changePassword" role="tabpanel" aria-labelledby="nav-home-tab">
+							  	<div class="row">
+		                <div class="col-md-12">
+		                  <h4>Change Password</h4>
+		                  <hr>
+		                  <form>
+                        <div class="form-group row">
+                          <label for="password" class="col-4 col-form-label">Password</label> 
+                          <div class="col-8">
+                            <input id="password" name="password" placeholder="Password" class="form-control here" type="password">
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <label for="newPassword" class="col-4 col-form-label">New Password</label> 
+                          <div class="col-8">
+                            <input id="newPassword" name="newPassword" placeholder="New Password" class="form-control here" type="password">
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <label for="confirmPassword" class="col-4 col-form-label">Confirm Password</label> 
+                          <div class="col-8">
+                            <input id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" class="form-control here" type="password">
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <div class="offset-4 col-8">
+                            <button name="saveChangePassword" type="submit" class="btn btn-primary">Save</button>
                           </div>
                         </div>
                       </form>
