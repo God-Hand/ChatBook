0aceeafd7a35bd8eab16a8aa7c517a76aca7dc54
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-06 05:06:37 +0530

    updated settings.php

--- includes/change_account_info.php
+++ includes/change_account_info.php
@@ -3,11 +3,11 @@
   require 'classes/User.php';
   if(isset($_POST['username'])) {
     $user = new User($conn, $_POST['username']);
-    if(isset($_POST['firstname']))
+    if(isset($_POST['firstname']) and !empty($_POST['firstname']))
     	$user->setFirstName($_POST['firstname']);
-    if(isset($_POST['lastname']))
+    if(isset($_POST['lastname']) and !empty($_POST['lastname']))
     	$user->setLastName($_POST['lastname']);
-    if(isset($_POST['bio']))
+    if(isset($_POST['bio']) and !empty($_POST['bio']))
     	$user->setBio($_POST['bio']);
     if(isset($_POST['email']) and $user->getEmail()!=$_POST['email'])
     	echo $user->changeEmail($_POST['email']);
