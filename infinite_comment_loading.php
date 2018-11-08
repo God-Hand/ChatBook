357d26bb8c0c250918d3b21c517c35ca1db0699b
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-08 23:40:42 +0530

    Update infinite_comment_loading.php

--- infinite_comment_loading.php
+++ infinite_comment_loading.php
@@ -1,112 +1,83 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
-	<style src="assets/css/comment.css"></style>
-	<style type="text/css">
-		div {
-	    word-wrap: break-word;
-		}
-	  .form-group{
-	    margin-bottom: 0px;
-	  }
-	  .col {
-	  	margin: -15px 0px;
-	  }
-	  .scrollClass {
-	    height:200px;
-	    overflow-y: scroll;
-	  }
-	  ::-webkit-scrollbar {
-	      width: 5px;
-	  }
-	  ::-webkit-scrollbar-track {
-	      background: #f1f1f1; 
-	  }
-	  ::-webkit-scrollbar-thumb {
-	      background: #888; 
-	  }
-	  ::-webkit-scrollbar-thumb:hover {
-	      background: #555; 
-	  }
-	  .image-size{
-			width: 40px;
-			margin-right:5px;
-		}
-		body{
-			font-size: 14px;
-		}
-		.button {
-			padding: 0px 4px;
-			margin-left: 5px;
-		}
-	</style>
-</head>
-<body style="background-color: #f9f9f9">
-	<div class="comment_area">
-  </div>
-  <img id="loading" src="assets/images/icons/loading.gif">
-</body>
+	<head>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
+		<style src="assets/css/comment.css"></style>
+		<style type="text/css">
+			div {
+		    word-wrap: break-word;
+			}
+		  .form-group{
+		    margin-bottom: 0px;
+		  }
+		  .col {
+		  	margin: -15px 0px;
+		  }
+		  .scrollClass {
+		    height:200px;
+		    overflow-y: scroll;
+		  }
+		  ::-webkit-scrollbar {
+		      width: 5px;
+		  }
+		  ::-webkit-scrollbar-track {
+		      background: #f1f1f1; 
+		  }
+		  ::-webkit-scrollbar-thumb {
+		      background: #888; 
+		  }
+		  ::-webkit-scrollbar-thumb:hover {
+		      background: #555; 
+		  }
+		  .image-size{
+				width: 40px;
+				margin-right:5px;
+			}
+			body{
+				font-size: 14px;
+			}
+			.button {
+				padding: 0px 4px;
+				margin-left: 5px;
+			}
+		</style>
+	</head>
+	<body class="bg-light">
+		<div class="comment_area">
+	  </div>
+	  <img id="loading" src="assets/images/icons/loading.gif">
+	</body>
 </html>
+
 <script>
 	$(document).ready(function() {
 		$('#loading').show();
-
-		//Original ajax request for loading first posts 
-		$.ajax({
-			url: "includes/load_comments.php",
-			type: "POST",
-			data: {
-				username : '<?php echo $_REQUEST['username']; ?>',
-			 	post_id: '<?php echo $_REQUEST['post_id']; ?>',
-			 	last_comment_id : 0
-			 },
-			cache:false,
-
-			success: function(data) {
-				$('#loading').hide();
-				$('.comment_area').html(data);
-			}
+		var post_id = '<?php echo $_REQUEST['post_id']; ?>';
+		$.post("includes/load_comments.php", {post_id : post_id, last_comment_id : 0}, function(data){
+			$('#loading').hide();
+			$('.comment_area').html(data);
 		});
 
 		$(window).scroll(function() {
-			// get id of last <div class='post' id='#post_id'></div>
 			var last_comment_id = $('.comment:last').attr('id');
 			var noMoreComments = $('.comment_area').find('.noMoreComments').val();
-
 			if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight) && noMoreComments == 'false') {
 				$('#loading').show();
-
-				var ajaxReq = $.ajax({
-					url: "includes/load_comments.php",
-					type: "POST",
-					data: {
-						username : '<?php echo $_REQUEST['username']; ?>', 
-						post_id: '<?php echo $_REQUEST['post_id']; ?>', 
-						last_comment_id : last_comment_id
-					},
-					cache:false,
-
-					success: function(response) {
-						$('.comment_area').find('.noMoreComments').remove(); //Removes current .nextpage 
-						$('.comment_area').find('.noMoreCommentsText').remove(); //Removes current .nextpage 
-
-						$('#loading').hide();
-						$('.comment_area').append(response);
-					}
+				$.post("includes/load_comments.php", {post_id : post_id, last_comment_id : last_comment_id}, function(data){
+					$('.comment_area').find('.noMoreComments').remove();
+					$('.comment_area').find('.noMoreCommentsText').remove();
+					$('#loading').hide();
+					$('.comment_area').append(response);
 				});
 			}
-			return false;
 		});
 	});
-</script>
 
-<script>
 	function likeComment(obj) {
 		if(obj.value == "0") {
 			obj.innerHTML = "<i class='fa fa-thumbs-down'></i>&nbspUnLike";
@@ -121,20 +92,7 @@
 		}
 	}
 	function saveAction(obj){
-		var user_logged_in = '<?php echo $_REQUEST['username']; ?>';
-		$.ajax({
-      type: "POST",
-      url: "includes/like_comment.php",
-      data: {
-        username : user_logged_in,
-				comment_id : obj.id,
-				user_action  : obj.value
-      },
-      success: function(result) {
-      },
-      error: function(result) {
-        alert('error');
-      }
-    });
+		$.post("includes/like_comment.php", {comment_id : obj.id, user_action : obj.value}, function(data){
+		});
 	}
 </script>
