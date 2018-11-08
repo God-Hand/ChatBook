86a42f8c405e69802c43cf7b14015d483746d9f3
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-09 00:13:53 +0530

    Update friend_request.php

--- friend_request.php
+++ friend_request.php
@@ -19,7 +19,10 @@
         <div class="card>">
           <div class="container card">
             <div class="row">
-              <?php include("infinite_friend_request_loading.php"); ?>
+              <div class="friend_request_area" style="width:100%;">
+              </div>
+              <br>
+              <img id="loading" src="assets/images/icons/loading.gif">
             </div>
           </div>
         </div>
@@ -28,3 +31,39 @@
   </div>
 </body>
 </html>
+<script>
+  $(document).ready(function() {
+    $('#loading').show();
+    $.post("includes/load_friend_requests.php", {last_request_id : 0}, function(data){
+      $('#loading').hide();
+      $('.friend_request_area').html(data);
+    });
+    $(window).scroll(function() {
+      var last_request_id = $('.request:last').attr('id');
+      var noMoreRequests = $('.friend_request_area').find('#noMoreRequests').val();
+      if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight) && noMoreRequests == 'false') {
+        $('#loading').show();
+        $.post("includes/load_friend_requests.php", {last_request_id : last_request_id}, function(data){
+          $('.friend_request_area').find('#noMoreRequests').remove();
+          $('.friend_request_area').find('#noMoreRequestsText').remove();
+          $('#loading').hide();
+          $('.friend_request_area').append(data);
+        });
+      }
+    });
+  });
+
+  function deleteRequest(obj){
+    var element = '.request#';
+    $(element.concat(obj.id)).fadeOut();
+  }
+  function acceptRequest(obj){
+    $.post("includes/accept_friend_request.php",{ user_from : obj.value }, function(e){
+      $('#totalfriendCounts').html(parseInt($('#totalfriendCounts').text())+1);
+    });
+  }
+  function rejectRequest(obj){
+    $.post("includes/reject_friend_request.php",{ user_from : obj.value }, function(e){
+    });
+  }
+</script>
