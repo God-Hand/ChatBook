841b9261c8088e4cd83b4d690888387d3284760b
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-09 21:07:31 +0530

    Update includes/load_messages.php

--- includes/load_messages.php
+++ includes/load_messages.php
@@ -14,15 +14,36 @@
 		$str = "";
 		$data_query = $message->getMessages($_POST['name'], $_POST['last_message_id'],$_POST['limit']);
 		if(mysqli_num_rows($data_query)!=0){
-			
+
 			while($row = mysqli_fetch_array($data_query)){
-				$message_by = "<a href='profile.php?profile_username=" . $user->getUsername() . "'>You</a>";
+
+				$message_body = replaceURLToLink($row['message_body']);
+				$message_time = $row['time'];
+				$now_date = date("Y-m-d H:i:s");
+				$message_datetime = new DateTime($message_time);
+				$now_datetime = new DateTime($now_date);
+				$message_time_in_text = getTimeFrame($now_datetime->diff($message_datetime));
+
 				if( $row['user_from'] == $user->getUsername()){
+					$message_by = "<a href='profile.php?profile_username=" . $user->getUsername() . "'>You</a>";
 					$str .= "<div class='row float-right ml-0 message'>
 										<div class='col-12 alert bg-success text-white'>
 											<div class='media'>
 											  <div class='media-body'>
 											    <h6 class='m-0'>" . $message_by . "<a id='message_id' class='btn btn-sm text-light text-secondary pr-0 float-right' onclick='deleteMessage(this)'><i class='fa fa-times'></i></a></h6>
+											    <p class='m-0'>" . $message_body . "</p>
+											    <small class='float-right'><em>" . $message_time_in_text . "</em></small>
+											  </div>
+											</div>
+										</div>
+									</div>";
+				} else {
+					$message_by = "<a href='profile.php?profile_username=" . $name . "'>" . $fullname . "</a>";
+					$str .= "<div class='row float-left mr-0 message'>
+										<div class='col-12 alert bg-primary text-white'>
+											<div class='media'>
+											  <div class='media-body'>
+											    <h6 class='m-0'>Jitendra Sharma</h6>
 											    <p class='m-0'>hi friend.
 											    	How are you? long time, no see
 											    </p>
@@ -33,19 +54,7 @@
 									</div>";
 				}
 			}
-			echo "<div class='row float-left mr-0 message'>
-				<div class='col-12 alert bg-primary text-white'>
-					<div class='media'>
-					  <div class='media-body'>
-					    <h6 class='m-0'>Jitendra Sharma</h6>
-					    <p class='m-0'>hi friend.
-					    	How are you? long time, no see
-					    </p>
-					    <small class='float-right'><em>12days ago</em></small>
-					  </div>
-					</div>
-				</div>
-			</div>
+			echo "
 			";
 		}
 	}
