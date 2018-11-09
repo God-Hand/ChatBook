481703e4f5a8fbd23efe592e7929a2c4a5aa0d65
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-10 02:30:24 +0530

    Update infinite_friends_loading.php

--- infinite_friends_loading.php
+++ infinite_friends_loading.php
@@ -31,17 +31,6 @@
 			$profile_button = "<div>
 												  <a href='profile.php?profile_username=" . $friend . "' class='btn btn-success btn-sm float-left'>See Profile</a>
 												</div>";
-			if ($user->getUsername() == $friend) {
-				$friend_button = "<button class='btn-over-friend-img btn btn-primary btn-sm float-right'><i class='fa fa-pencil'></i>&nbsp;Edit</button>";
-			}else{
-				if ($user->isFriend($friend)){
-					$friend_button = "<button id='" . $friend ."' class='btn btn-danger btn-sm btn-over-friend-img float-right addfriend' onclick='friend(this)' value='0' onmouseleave='friendAction(this)'>Remove Friend</button>";
-				} elseif ($request->didSendRequest($friend) == 1) {
-					$friend_button = "<button id='" . $friend . "' class='btn btn-warning btn-sm btn-over-friend-img float-right addfriend' onclick='friend(this)' value='2' onmouseleave='friendAction(this)'>Cancel Request</button>";
-				} else {
-					$friend_button = "<button id='" . $friend . "' class='btn btn-success btn-sm btn-over-friend-img float-right addfriend' onclick='friend(this)' value='1' onmouseleave='friendAction(this)'>Add Friend</button>";
-				}
-			}
 			$str .= "<div class='col-md-6'>
 								<div class='card profile-card shadow p-3 mb-4 bg-white rounded'>
 							    <div class='profile-card-img-block'>
@@ -49,7 +38,6 @@
 							      	" . $friend_bio . "
 							      </div>
 							      <img class='rounded cover-img' src='" . $friend_cover_pic . "' id='profilecoverimage'>
-							      " . $friend_button . "
 							    </div>
 									<div class='media friend-profile-media'>
 									  <img class='mr-1 padding-5-circle friend-profile-pic' src='" . $friend_profile_pic . "' alt='username'>
