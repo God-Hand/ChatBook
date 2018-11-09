e9a1f5194283be937d71d4791be7fc354cfa3df3
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-10 03:15:33 +0530

    save

--- profile.php
+++ profile.php
@@ -13,51 +13,51 @@
 		<meta charset="utf-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
-	<style type="text/css">
-		/*Profile card*/
-		.profile-card .profile-card-img-block{
-		    float:left;
-		    width:100%;
-		    height:150px;
-		    overflow:hidden;
-		}
-		.profile-card .profile-card-body{
-		    position:relative;
-		}
-		.profile-card .profile {
-		    position: absolute;
-		    top: -62px;
-		    left: 50%;
-		    width:100px;
-		    margin-left: -50px;
-		}
-		.profile-card .profile-card-img-block{
-		    position:relative;
-		}
-		.profile-card .profile-card-img-block .btn-over-img{
-			position: relative;
-			top: -140px;
-	    right: 10px;
-		}
-		.profile-card .profile-card-img-block > .profile-info-box{
-		    position:absolute;
-		    width:100%;
-		    height:100%;
-		    color:#fff;
-		    padding:20px;
-		    text-align:center;
-		    font-size:14px;
-		   -webkit-transition: 1s ease;
-		    transition: 1s ease;
-		    opacity:0;
-		}
-		.profile-card .profile-card-img-block:hover > .profile-info-box{
-				border-radius: .25rem!important;
-		    opacity:1;
-		    -webkit-transition: all 1s ease;
-		    transition: all 1s ease;
-		}
-	</style>
+<style type="text/css">
+	/*Profile card*/
+	.profile-card .profile-card-img-block{
+	    float:left;
+	    width:100%;
+	    height:150px;
+	    overflow:hidden;
+	}
+	.profile-card .profile-card-body{
+	    position:relative;
+	}
+	.profile-card .profile {
+	    position: absolute;
+	    top: -62px;
+	    left: 50%;
+	    width:100px;
+	    margin-left: -50px;
+	}
+	.profile-card .profile-card-img-block{
+	    position:relative;
+	}
+	.profile-card .profile-card-img-block .btn-over-img{
+		position: relative;
+		top: -140px;
+    right: 10px;
+	}
+	.profile-card .profile-card-img-block > .profile-info-box{
+	    position:absolute;
+	    width:100%;
+	    height:100%;
+	    color:#fff;
+	    padding:20px;
+	    text-align:center;
+	    font-size:14px;
+	   -webkit-transition: 1s ease;
+	    transition: 1s ease;
+	    opacity:0;
+	}
+	.profile-card .profile-card-img-block:hover > .profile-info-box{
+			border-radius: .25rem!important;
+	    opacity:1;
+	    -webkit-transition: all 1s ease;
+	    transition: all 1s ease;
+	}
+</style>
 <body>
 	<div role="main" class="container">
 		<div class="row">
@@ -73,9 +73,9 @@
 				    		echo "<a href='settings.php' class='btn-over-img btn btn-primary float-right'><i class='fa fa-pencil'></i>&nbsp;Edit</a>";
 				    	} else {
 				    		if ($user->isFriend($profile_user->getUsername())){
-									$friend_button = "<button id='" . $profile_user->getUsername() . "' class='btn-over-img btn btn-sm btn-danger float-right addfriend' onclick='friend(this);friendAction(this);reload()' value='0'>Remove Friend</button>";
+									$friend_button = "<button id='" . $profile_user->getUsername() . "' class='btn-over-img btn btn-sm btn-danger float-right addfriend' onclick='friend(this);friendAction(this);reload();' value='0'>Remove Friend</button>";
 								} elseif ($request->didReceiveRequest($profile_user->getUsername()) == 1) {
-									$friend_button = "<button id='" . $profile_user->getUsername() . "' class='btn-over-img btn btn-sm btn-success float-right addfriend' onclick='friend(this);friendAction(this);' value='1'>Accept Request</button>";
+									$friend_button = "<button id='" . $profile_user->getUsername() . "' class='btn-over-img btn btn-sm btn-success float-right addfriend' onclick='friend(this);friendAction(this);reload();' value='1'>Accept Request</button>";
 								} elseif ($request->didSendRequest($profile_user->getUsername()) == 1) {
 									$friend_button = "<button id='" . $profile_user->getUsername() . "' class='btn-over-img btn btn-sm btn-warning float-right addfriend' onclick='friend(this);friendAction(this);' value='2'>Cancel Request</button>";
 								} else {
@@ -116,6 +116,7 @@
 				    <a class="nav-link" data-toggle="tab" href="#friend_box">Friends</a>
 				  </li>
 				  <?php
+				  	// show this tab when profile_user is not main user
 				  	if($user->getUsername() != $profile_user->getUsername()){
 				  		echo "<li class='nav-item'>
 									    <a class='nav-link' data-toggle='tab' href='#message_box'>Message</a>
@@ -125,7 +126,9 @@
 				</ul>
 				<!-- Tab panes -->
 				<div class="tab-content mb-2">
-				  <div class="tab-pane container-fluid active" id="profile_post_box">
+
+				  <!--- post by profile_user and user--->
+				  <div class="tab-pane active" id="profile_post_box">
 				  	<div class="card shadow p-3 mb-4 bg-white rounded post_block">
 						  <div class="form-group">
 						    <textarea id="postBody" class="form-control border border-primary" rows="5" maxlength="60000" id="post" placeholder="Post Something here..."></textarea>
@@ -145,9 +148,13 @@
 						</div>
 						<img id="loading" src="assets/images/icons/loading.gif">
 				  </div>
+
+				  <!---profile_user friend_list--->
 				  <div class="tab-pane container-fluid fade" id="friend_box">
 				  	<?php include 'infinite_friends_loading.php' ?>
 				  </div>
+
+				  <!---message tab for other user's not for profile_user--->
 				  <?php
 				  	if($user->getUsername() != $profile_user->getUsername()){
 				  		echo "<div class='tab-pane container-fluid fade' id='message_box'>
@@ -155,6 +162,7 @@
 									  </div>";
 				  	} 
 				  ?>
+
 				</div>
 			</div>
 		</div>
