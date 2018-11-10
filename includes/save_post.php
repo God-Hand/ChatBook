8d77bc1aeddf23166607a5dcb295f4ff05610394
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-10 18:51:05 +0530

    Update includes/save_post.php

--- includes/save_post.php
+++ includes/save_post.php
@@ -2,11 +2,15 @@
   require '../config/config.php';
   require 'classes/Post.php';
   require 'classes/User.php';
+  require 'classes/Notification.php';
   require '../functions/timeframe_function.php';
   require '../functions/text_filter.php';
   
   if((isset($_POST['imageLocation']) or isset($_POST['postBody']))){
     $post = new Post($conn, $_SESSION['username']);
+    $notification = new Notification($conn, $_SESSION['username']);
+    $user = new User($conn, $_SESSION['username']);
+    $friends = $user->getFriendArray();
     if (isset($_POST['imageLocation']) and isset($_POST['postBody'])){ 
       $post_body = removeSpaces($_POST['postBody']);
       $post_body = secureText($conn, $post_body);
