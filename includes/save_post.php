997a4b35ba15b431fd17f6356eb2168fed9236e7
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-10 19:14:57 +0530

    Update includes/save_post.php

--- includes/save_post.php
+++ includes/save_post.php
@@ -26,6 +26,20 @@
       $post->addPost('', '', $_POST['imageLocation']);
     }
 
-    
+    $type = "post";
+    if (isset($_POST['userTo']) and !empty($_POST['userTo'])){
+      $notification_body = "<a href='profile.php?profile_username=" . $user->getUsername() . "' class='text-primary'>" .$user->getUsername() . "</a> Share a post on your profile";
+      $link = "profile.php?profile_username=" . $user->getUsername();
+      $notification->sendNotification($_POST['userTo'], $notification_body, $type, $link)
+    } else {
+      $notification_body = "<a href='profile.php?profile_username=" . $user->getUsername() . "' class='text-primary'>" .$user->getUsername() . "</a> Share a post";
+      $link = "index.php?type=post&post_id=" . $;
+      $friends = $user->getFriendArray();
+      foreach ($friends as $friend) {
+        if ($friend != ''){
+          $notification->sendNotification($friend, $notification_body, $type, $link);
+        }
+      }
+    }
   }
 ?>
