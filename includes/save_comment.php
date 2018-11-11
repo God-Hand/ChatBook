48da5572c0c8a4d4f19783fb88eaf12eea0b32a2
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:57:59 +0530

    Update includes/save_comment.php

--- includes/save_comment.php
+++ includes/save_comment.php
@@ -1,15 +1,16 @@
 <?php
-	require '../config/config.php';
-	require 'classes/Comment.php';
-	require '../functions/text_filter.php';
-
-	$username = $_SESSION['username'];
-	$post_id = $_POST['post_id'];
-	$comment_body  = removeSpaces($_POST['comment_body']);
-	$comment_body = secureText($conn , $comment_body);
-	$comment = new Comment($conn, $username);
-	if ($comment_body != '') {
-		$comment->addComment($comment_body, $post_id);
+	if(isset($_POST['post_id']) and $_POST['comment_body']){
+		require '../config/config.php';
+		require 'classes/Comment.php';
+		require '../functions/text_filter.php';
+		$username = $_SESSION['username'];
+		$post_id = $_POST['post_id'];
+		$comment_body  = removeSpaces($_POST['comment_body']);
+		$comment_body = secureText($conn , $comment_body);
+		$comment = new Comment($conn, $username);
+		if ($comment_body != '') {
+			$comment->addComment($comment_body, $post_id);
+		}
+		echo $comment->getNumOfComments($post_id);
 	}
-	echo $comment->getNumOfComments($post_id);
 ?>
