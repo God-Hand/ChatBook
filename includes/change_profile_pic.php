15273499e85d79b068fdef058bb4bd511b77753e
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:26:19 +0530

    Update includes/change_profile_pic.php

--- includes/change_profile_pic.php
+++ includes/change_profile_pic.php
@@ -2,7 +2,6 @@
   if(isset($_POST['uploadedProfilePic'])) {
 		require '../config/config.php';
 	  require 'classes/User.php';
-	  
     $user = new User($conn, $_SESSION['username']);
     $user->uploadProfilePic($_POST['uploadedProfilePic']);
   }
