8eb02cd9dea277c94579f87604fd0f6338e1b819
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:35:46 +0530

    Update includes/like_post.php

--- includes/like_post.php
+++ includes/like_post.php
@@ -1,13 +1,10 @@
 <?php
-
-	require '../config/config.php';
-	require 'classes/PostLike.php';
-
-	$username = $_SESSION['username'];
-	$post_id = $_POST['post_id'];
-	$user_action  = $_POST['user_action'];
-	if($user_action != 2){
-		$post_like = new PostLike($conn, $username);
-		$post_like->likeThePost($post_id, $user_action);
+	if(isset($_POST['post_id']) and isset($_POST['user_action'])){
+		require '../config/config.php';
+		require 'classes/PostLike.php';
+		if($_POST['user_action'] != 2){
+			$post_like = new PostLike($conn, $_SESSION['username']);
+			$post_like->likeThePost($_POST['post_id'], $_POST['user_action']);
+		}
 	}
 ?>
