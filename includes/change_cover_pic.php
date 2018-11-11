8bc7e97c7296ec462c9a5627ba9e57565457c840
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:22:53 +0530

    Update includes/change_cover_pic.php

--- includes/change_cover_pic.php
+++ includes/change_cover_pic.php
@@ -2,6 +2,7 @@
   if(isset($_POST['uploadedCoverPic'])) {
 		require '../config/config.php';
 	  require 'classes/User.php';
+	  
     $user = new User($conn, $_SESSION['username']);
     $user->uploadCoverPic($_POST['uploadedCoverPic']);
   }
