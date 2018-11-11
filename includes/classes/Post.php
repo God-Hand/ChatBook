1dd8923bed3f6ac56b337f4c6fa75da1dda8830a
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 15:11:44 +0530

    Update includes/classes/Post.php

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -17,35 +17,29 @@
 		private $username;
 		private $conn;
 
-		// construct post object for given username
 		public function __construct($conn, $username){
 			$this->conn = $conn;
 			$this->username = $username;
 		}
 
-		// insert post by the user
 		public function addPost( $user_to, $body, $target_file='') {
 			$query = mysqli_query($this->conn, "INSERT INTO posts VALUES (DEFAULT, '$this->username', '$user_to', '$body', '$target_file', DEFAULT, DEFAULT)");
 			return mysqli_insert_id($this->conn);
 		}
 
-		// get the post by its id
 		public function getPost($post_id) {
 			$query = mysqli_query($this->conn, "SELECT * FROM posts WHERE id='$post_id' AND deleted=0");
 			return $query;
 		}
 
-		// delete post
 		public function deletePost($post_id) {
 			$query = mysqli_query($this->conn, "UPDATE posts SET deleted=1 WHERE post_id='$post_id'");
 		}
 
-		// delete post
 		public function deleteAllPosts() {
 			$query = mysqli_query($this->conn, "UPDATE posts SET deleted=1 WHERE user_to='$this->username' OR user_from='$this->username'");
 		}
 
-		// load posts, where post_id< given post_id and specific limit 
 		public function loadAllPosts($last_post_id, $limit) {
 			if ( $last_post_id == 0 ){
 				$query = mysqli_query($this->conn, "SELECT * FROM posts WHERE deleted=0 ORDER BY post_id DESC LIMIT $limit");
@@ -55,7 +49,6 @@
 			return $query;
 		}
 
-		// load posts, for current login user 
 		public function loadUserProfilePosts($last_post_id, $limit) {
 			if ( $last_post_id == 0 ){
 				$query = mysqli_query($this->conn, "SELECT * FROM posts WHERE deleted=0 AND (user_from='$this->username' OR user_to='$this->username') ORDER BY post_id DESC LIMIT $limit");
@@ -65,7 +58,6 @@
 			return $query;
 		}
 
-		// load posts, of a particular user
 		public function loadProfilePosts($last_post_id, $name, $limit) {
 			if ( $last_post_id == 0 ){
 				$query = mysqli_query($this->conn, "SELECT * FROM posts WHERE deleted=0 AND ((user_from='$this->username' AND user_to='$name') OR (user_from='$name' AND (user_to='' OR user_to='$this->username'))) ORDER BY post_id DESC LIMIT $limit");
