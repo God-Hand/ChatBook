7c77b0e8f43cc77c4d49ed06fded4361e841409a
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:27:36 +0530

    Update includes/delete_all_messages.php

--- includes/delete_all_messages.php
+++ includes/delete_all_messages.php
@@ -2,7 +2,6 @@
 	if(isset($_POST['name'])){
 		require '../config/config.php';
 		require 'classes/Message.php';
-
 		$message = new Message($conn, $_SESSION['username']);
 		$message->deleteAllMessagesTo($_POST['name']);
 	}
