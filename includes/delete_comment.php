fea7c409c9d6cb3ba33283bb55e0b3bf50246458
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:27:49 +0530

    Update includes/delete_comment.php

--- includes/delete_comment.php
+++ includes/delete_comment.php
@@ -1,7 +1,6 @@
 <?php
 	require '../config/config.php';
 	require 'classes/Comment.php';
-
 	$comment = new Comment($conn, $_SESSION['username']);
 	$comment->deleteComment($_POST['comment_id']);
 	echo $comment->getCommentPostId($_POST['comment_id']);
