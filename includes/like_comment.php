f29e84ee2e34ef2669b55afaa950bc7eab1cb60d
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:35:39 +0530

    Update includes/like_comment.php

--- includes/like_comment.php
+++ includes/like_comment.php
@@ -2,7 +2,7 @@
 	if(isset($_POST['comment_id']) and isset($_POST['user_action'])){
 		require '../config/config.php';
 		require 'classes/CommentLike.php';
-		if($user_action != 2){
+		if($_POST['user_action'] != 2){
 			$comment_like = new CommentLike($conn, $_SESSION['username']);
 			$comment_like->likeTheComment($_POST['comment_id'], $_POST['user_action']);
 		}
