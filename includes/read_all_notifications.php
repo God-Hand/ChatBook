6aa50be73a287ba892a9930f2812be1e39db24c3
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:56:20 +0530

    Update includes/read_all_notifications.php

--- includes/read_all_notifications.php
+++ includes/read_all_notifications.php
@@ -2,7 +2,6 @@
 	if(isset($_POST['first'])){
 		require '../config/config.php';
 	  require 'classes/Notification.php';
-
 	  $notification = new Notification($conn, $_SESSION['username']);
 	  $notification->readAllNotifications($_POST['first']);
 	}
