9b715a31951d9f37d4a8751462c2f5d78a74eab0
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:22:39 +0530

    Update includes/accept_friend_request.php

--- includes/accept_friend_request.php
+++ includes/accept_friend_request.php
@@ -3,7 +3,6 @@
 		require '../config/config.php';
 		require 'classes/User.php';
 		require 'classes/FriendRequest.php';
-		
 		$user = new User($conn, $_SESSION['username']);
 		$request = new FriendRequest($conn, $_SESSION['username']);
 		if($request->acceptRequest($_POST['user_from'])){
