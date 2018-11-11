8f3c2cd6a32f07edf0ab111f41e5ba6f8fe46310
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:19:21 +0530

    Update includes/change_password.php

--- includes/change_password.php
+++ includes/change_password.php
@@ -2,6 +2,7 @@
   if(isset($_POST['password']) and isset($_POST['newpassword'])){
 		require '../config/config.php';
 	  require 'classes/User.php';
+	  
   	$user = new User($conn, $_SESSION['username']);
   	echo $user->changePassword($_POST['password'], $_POST['newpassword']);
   }
