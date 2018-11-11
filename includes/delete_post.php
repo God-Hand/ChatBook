7bda6eecc3f6b9bfa0997f843dc7aefcb163a3a9
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:29:55 +0530

    Update includes/delete_post.php

--- includes/delete_post.php
+++ includes/delete_post.php
@@ -1,13 +1,14 @@
 <?php
-	require '../config/config.php';
-	require 'classes/Post.php';
-	require 'classes/Comment.php';
-  require 'classes/Notification.php';
-
-	$post = new Post($conn, $_SESSION['username']);
-	$comment = new Comment($conn, $_SESSION['username']);
-  $notification = new Notification($conn, $_SESSION['username']);
-	$post->deletePost($_POST['post_id']);
-	$comment->deleteComments($_POST['post_id']);
-	$notification->deleteNotifications($_POST['post_id']);
+	if(isset($_POST['post_id'])){
+		require '../config/config.php';
+		require 'classes/Post.php';
+		require 'classes/Comment.php';
+	  require 'classes/Notification.php';
+		$post = new Post($conn, $_SESSION['username']);
+		$comment = new Comment($conn, $_SESSION['username']);
+	  $notification = new Notification($conn, $_SESSION['username']);
+		$post->deletePost($_POST['post_id']);
+		$comment->deleteComments($_POST['post_id']);
+		$notification->deleteNotifications($_POST['post_id']);
+	}
 ?>
