93fcc9eba8aa8f386a836fb0fff8efdb0fa75489
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:38:48 +0530

    Update includes/load_friend_request_count.php

--- includes/load_friend_request_count.php
+++ includes/load_friend_request_count.php
@@ -1,7 +1,6 @@
 <?php
 	require '../config/config.php';
 	require 'classes/FriendRequest.php';
-
 	$request = new FriendRequest($conn, $_SESSION['username']);
 	$count = $request->getFriendRequestsCount();
 	if($count > 99){
