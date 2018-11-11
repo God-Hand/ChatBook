4975704a1d985a16b20a0616527023c073470512
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 12:27:16 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -142,7 +142,35 @@
 								if( $location != ''){
 									$location = "<tr class='table-light'><td>Address </td><td>- " . $location . "</td></tr>";
 								}
-								echo "<table class='table'>
+								echo "<table class='table table-borderless table-hover'>
+  <thead>
+    <tr>
+      <th scope='col'>#</th>
+      <th scope='col'>First</th>
+      <th scope='col'>Last</th>
+      <th scope='col'>Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope='row'>1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope='row'>2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope='row'>3</th>
+      <td colspan='2'>Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table><table class='table table-borderless'>
 												<thead><tr>Personal Info :</tr></thead><tbody>
 												" . $gender . $birthday . $phone_no . $location ."
 											</tbody></table>";
