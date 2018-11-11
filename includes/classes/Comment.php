cc1a5beec0910f58208232bd92934e05cf9bfffc
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 15:20:31 +0530

    Update includes/classes/Comment.php

--- includes/classes/Comment.php
+++ includes/classes/Comment.php
@@ -1,4 +1,21 @@
 <?php
+	/**
+	class Comment {
+		private $username;
+		private $conn;
+
+		__construct($conn, $username) : construct comment object for given username
+		addComment( $body, $post_id)  : insert comment by the user
+		getComment($comment_id)  			: get the comment by its id
+		deleteComment($comment_id) 		: delete comment
+		deleteAllComments()  					: delete all comments by user
+		deleteComments($post_id) 			: delete comments for a given post_id
+		loadUserComments()   					: return those comments, which are commented_by user
+		loadAllComments($post_id, $last_comment_id, $limit) : load posts, where comment_id< given comment_id and post_id=$post_id , limit='$limit'
+		getNumOfComments($post_id)  	: return number of comments by the user
+		getCommentPostId($comment_id) : return post_id for a comment_id
+	}
+	*/
 	class Comment {
 		private $username;
 		private $conn;
@@ -8,53 +25,46 @@
 			$this->conn = $conn;
 			$this->username = $username;
 		}
-
-		// insert comment by the user
+		
 		public function addComment( $body, $post_id) {
-			$query = mysqli_query($this->conn, "INSERT INTO comments VALUES (default, '$this->username', '$body', DEFAULT, DEFAULT, DEFAULT, '$post_id')");
+			$query = mysqli_query($this->conn, "INSERT INTO comments VALUES (default, '$this->username', '$body', DEFAULT, DEFAULT, '$post_id')");
 		}
 
-		// get the comment by its id
 		public function getComment($comment_id) {
-			$query = mysqli_query($this->conn, "SELECT * FROM comments WHERE comment_id='$comment_id' AND  deleted=0 AND user_closed=0");
+			$query = mysqli_query($this->conn, "SELECT * FROM comments WHERE comment_id='$comment_id' AND  deleted=0");
 			return $query;
 		}
 
-		// delete comment
 		public function deleteComment($comment_id) {
 			$query = mysqli_query($this->conn, "UPDATE comments SET deleted=1 WHERE comment_id='$comment_id'");
 		}
 
-		// delete all comments by user
 		public function deleteAllComments() {
 			$query = mysqli_query($this->conn, "UPDATE comments SET deleted=1 WHERE commented_by='$this->username'");
 		}
 
-		// delete comments
 		public function deleteComments($post_id) {
 			$query = mysqli_query($this->conn, "UPDATE comments SET deleted=1 WHERE post_id='$post_id'");
 		}
 
-		// return those comments, which are commented_by user
 		public function loadUserComments() {
 			$query = mysqli_query($this->conn, "SELECT * FROM comments WHERE commented_by='$this->username' AND deleted=0 ORDER BY comment_id DESC");
 			return $query;
 		}
 
-		// load posts, where comment_id< given comment_id and post_id=$post_id , limit='$limit'
 		public function loadAllComments($post_id, $last_comment_id, $limit) {
 			if ( $last_comment_id == 0 ){
-				$query = mysqli_query($this->conn, "SELECT * FROM comments WHERE post_id='$post_id' AND deleted=0 AND user_closed=0 ORDER BY comment_id DESC LIMIT $limit");
+				$query = mysqli_query($this->conn, "SELECT * FROM comments WHERE post_id='$post_id' AND deleted=0 ORDER BY comment_id DESC LIMIT $limit");
 			} else {
-				$query = mysqli_query($this->conn, "SELECT * FROM comments WHERE comment_id<'$last_comment_id' AND post_id='$post_id' AND deleted=0 AND user_closed=0 ORDER BY comment_id DESC LIMIT $limit");
+				$query = mysqli_query($this->conn, "SELECT * FROM comments WHERE comment_id<'$last_comment_id' AND post_id='$post_id' AND deleted=0 ORDER BY comment_id DESC LIMIT $limit");
 			}
 			return $query;
 		}
 
-		// return number of comments by the user
 		public function getNumOfComments($post_id) {
-			$query = mysqli_query($this->conn, "SELECT * FROM comments WHERE commented_by='$this->username' AND post_id='$post_id' AND deleted=0 AND user_closed=0");
-			return mysqli_num_rows($query);
+			$query = mysqli_query($this->conn, "SELECT count(*) as commentcount FROM comments WHERE commented_by='$this->username' AND post_id='$post_id' AND deleted=0");
+			$row = mysqli_fetch_array($query);
+			return $row['commentcount'];
 		}
 
 		// return post_id for a comment_id
