b8151826a0e8787178071e1ecb5803760afd03ac
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 15:04:52 +0530

    Update includes/classes/PostLike.php

--- includes/classes/PostLike.php
+++ includes/classes/PostLike.php
@@ -1,21 +1,30 @@
 <?php
+	/**
+	class PostLike {
+		private $conn;
+		private $username;
+		__construct($conn, $username) : create postLike object to like and dislike operation
+		postLikesCount($post_id) 			: get post likes 
+		isUserLikePost($post_id) 			: check wheather user likes the post or not
+		deleteAllLikes() 							: delete like by user
+		likeThePost($post_id, $value) : like the given post_id
+	}
+	*/
 	class PostLike {
 		private $conn;
 		private $username;
 
-		// create postLike object to like and dislike operation
 		function __construct($conn, $username) {
 			$this->conn = $conn;
 			$this->username = $username;
 		}
 
-		// get post likes 
 		public function postLikesCount($post_id) {
-			$query = mysqli_query($this->conn, "SELECT * FROM post_likes WHERE post_id='$post_id' AND liked=1");
-			return mysqli_num_rows($query);
+			$query = mysqli_query($this->conn, "SELECT count(*) as postlikescount FROM post_likes WHERE post_id='$post_id' AND liked=1");
+			$row = mysqli_fetch_array($query);
+			return $row['postlikescount'];
 		}
 
-		// check wheather user likes the post or not
 		public function isUserLikePost($post_id) {
 			$query = mysqli_query($this->conn, "SELECT liked FROM post_likes WHERE post_id='$post_id' AND username='$this->username'");
 			if (mysqli_num_rows($query) != 0) {
@@ -25,12 +34,10 @@
 			return 0;
 		}
 
-		// delete like by user
 		public function deleteAllLikes(){
 			$query = mysqli_query($this->conn, "DELETE FROM post_likes WHERE username='$this->username'");
 		}
 
-		// like the given post_id
 		public function likeThePost($post_id, $value) {
 			$delete_query = mysqli_query($this->conn, "DELETE FROM post_likes WHERE post_id='$post_id' AND username='$this->username'");
 			$insert_query = mysqli_query($this->conn, "INSERT INTO post_likes Values ('$this->username', '$value','$post_id')");
