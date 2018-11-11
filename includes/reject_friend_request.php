456cc36472c5655466612b1874e80644ce97921a
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 20:56:41 +0530

    Update includes/reject_friend_request.php

--- includes/reject_friend_request.php
+++ includes/reject_friend_request.php
@@ -2,7 +2,6 @@
 	require '../config/config.php';
 	require 'classes/FriendRequest.php';
 	if(isset($_POST['user_from'])) {
-		
 		$request = new FriendRequest($conn, $_SESSION['username']);
 		$request->rejectRequest($_POST['user_from']);
 	}
