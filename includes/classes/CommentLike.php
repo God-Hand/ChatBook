48a5b7066276dd1455c1c143b93068c741bf2c37
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-11 15:29:02 +0530

    Update includes/classes/CommentLike.php

--- includes/classes/CommentLike.php
+++ includes/classes/CommentLike.php
@@ -1,21 +1,30 @@
 <?php
+	/**
+	class CommentLike {
+		private $conn;
+		private $username;
+		__construct($conn, $username) 			: create commentLike object to like and dislike operation
+		commentLikesCount($comment_id) 			: get comment likes 
+		isUserLikeComment($comment_id) 			: check wheather user likes the comment or not
+		deleteAllLikes()										: delete like by user
+		likeTheComment($comment_id, $value) : like the given comment_id
+	}
+	*/
 	class CommentLike {
 		private $conn;
 		private $username;
 
-		// create commentLike object to like and dislike operation
 		function __construct($conn, $username) {
 			$this->conn = $conn;
 			$this->username = $username;
 		}
 
-		// get comment likes 
 		public function commentLikesCount($comment_id) {
-			$query = mysqli_query($this->conn, "SELECT * FROM comment_likes WHERE comment_id='$comment_id' AND liked=1");
-			return mysqli_num_rows($query);
+			$query = mysqli_query($this->conn, "SELECT count(*) as commentlikecount FROM comment_likes WHERE comment_id='$comment_id' AND liked=1");
+			$row = mysqli_fetch_array($query);
+			return $row['commentlikecount'];
 		}
 
-		// check wheather user likes the comment or not
 		public function isUserLikeComment($comment_id) {
 			$query = mysqli_query($this->conn, "SELECT liked FROM comment_likes WHERE comment_id='$comment_id' AND username='$this->username'");
 			if (mysqli_num_rows($query) != 0) {
@@ -25,12 +34,10 @@
 			return 0;
 		}
 
-		// delete like by user
 		public function deleteAllLikes(){
 			$query = mysqli_query($this->conn, "DELETE FROM comment_likes WHERE username='$this->username'");
 		}
 
-		// like the given comment_id
 		public function likeTheComment($comment_id, $value) {
 			$delete_query = mysqli_query($this->conn, "DELETE FROM comment_likes WHERE comment_id='$comment_id' AND username='$this->username'");
 			$insert_query = mysqli_query($this->conn, "INSERT INTO comment_likes Values ('$this->username', '$value','$comment_id')");
