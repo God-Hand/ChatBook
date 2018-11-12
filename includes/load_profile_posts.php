95342673c7acb01b2419247233f47500bc489034
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-12 19:56:43 +0530

    Update includes/load_profile_posts.php

--- includes/load_profile_posts.php
+++ includes/load_profile_posts.php
@@ -98,7 +98,7 @@
 					    </div>
 					  </div>
 					  <iframe class='commentframe' id='commentframe" . $post_id . "' src='" . $comment_frame_url . "'></iframe>
-					  <div class='input-group mb-3' style='margin:5px 0px;'>
+					  <div class='input-group'>
 					    <input id='comment" . $post_id . "' type='text' class='form-control border border-primary' placeholder='Comment...'>
 					    <div class='input-group-append' maxlength='250'>
 					      <button id='" . $post_id . "' class='btn btn-primary sendComment" . $post_id . "' type='button' onclick='sendComment(this)'><i class='fa fa-paper-plane' aria-hidden='true'></i></button>
