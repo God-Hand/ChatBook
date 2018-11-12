1a286dded8c4de204b51001db76238959878a3ed
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-12 20:11:02 +0530

    Update friend_request.php

--- friend_request.php
+++ friend_request.php
@@ -33,22 +33,33 @@
 </body>
 </html>
 <script>
+  var friendRequestResponse = true;
+  
+  function loadRequest(){
+    var last_request_id = $('.request:last').attr('id');
+    var noMoreRequests = $('.friend_request_area').find('#noMoreRequests').val();
+    if (friendRequestResponse && noMoreRequests == 'false') {
+      friendRequestResponse = false;
+      $('#loading').show();
+      $.post("includes/load_friend_requests.php", {last_request_id : last_request_id}, function(data){
+        $('.friend_request_area').find('#noMoreRequests').remove();
+        $('#loading').hide();
+        $('.friend_request_area').append(data);
+        friendRequestResponse = true;
+      });
+    }
+  }
   $(document).ready(function() {
+    friendRequestResponse = false;
     $('#loading').show();
     $.post("includes/load_friend_requests.php", {last_request_id : 0}, function(data){
       $('#loading').hide();
       $('.friend_request_area').html(data);
+      friendRequestResponse = true;
     });
     $(window).scroll(function() {
-      var last_request_id = $('.request:last').attr('id');
-      var noMoreRequests = $('.friend_request_area').find('#noMoreRequests').val();
-      if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight) && noMoreRequests == 'false') {
-        $('#loading').show();
-        $.post("includes/load_friend_requests.php", {last_request_id : last_request_id}, function(data){
-          $('.friend_request_area').find('#noMoreRequests').remove();
-          $('#loading').hide();
-          $('.friend_request_area').append(data);
-        });
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        loadRequest();
       }
     });
   });
