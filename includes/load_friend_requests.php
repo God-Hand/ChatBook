d0dc3a80600d1e051a971a73b901d6bb81b0b144
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-12 20:13:40 +0530

    Update includes/load_friend_requests.php

--- includes/load_friend_requests.php
+++ includes/load_friend_requests.php
@@ -24,42 +24,38 @@
 
 				if ($user_to == $user->getUsername()){
 					$user_from_obj = new User($conn, $user_from);
-					$str .= "<div id='" . $request_id . "' class='p-3 col-12 request'>
-			              <div class='card' id='id'>
-			                <div class='card-body media'>
-				                <a href='profile.php?profile_username=". $user_from ."' class='text-primary'><img src='" . $user_from_obj->getProfilePic() . "' alt='profile-image' class='padding-5-circle' style='width:60px;height:60px;'/>
-			                  <div class='media-body pl-1'>
-			                    <h6>" . $user_from_obj->getFirstAndLastName() . "</h6></a><small class='text-muted'>
-			                    <em>" . $user->getMutualFriendsCount($user_from) . " mutual friends</em></small>
-			                  </div>
-			                  <div class='form-row' style='width:30%;'>
-			                    <div class='col-xs-6'>
-			                      <button id='" . $request_id . "' class='btn btn-sm btn-success' value='" . $user_from . "' onclick='acceptRequest(this);deleteRequest(this)' value='1'>Accept</button>
-			                    </div>
-			                    <div class='col-xs-6'>
-			                      <button id='" . $request_id . "' class='btn btn-sm width-60 btn-danger' value='" . $user_from . "' onclick='rejectRequest(this);deleteRequest(this);'> Reject</button>
-			                    </div>
-			                  </div>
-			                </div>
-			              </div>
+					$str .= "<div id='" . $request_id . "' class='p-3 col-12 request m-3 card'>
+		                <div class='card-body media'>
+			                <a href='profile.php?profile_username=". $user_from ."' class='text-primary'><img src='" . $user_from_obj->getProfilePic() . "' alt='profile-image' class='padding-5-circle' style='width:60px;height:60px;'/>
+		                  <div class='media-body pl-1'>
+		                    <h6>" . $user_from_obj->getFirstAndLastName() . "</h6></a><small class='text-muted'>
+		                    <em>" . $user->getMutualFriendsCount($user_from) . " mutual friends</em></small>
+		                  </div>
+		                  <div class='form-row' style='width:30%;'>
+		                    <div class='col-xs-6'>
+		                      <button id='" . $request_id . "' class='btn btn-sm btn-success' value='" . $user_from . "' onclick='acceptRequest(this);deleteRequest(this)' value='1'>Accept</button>
+		                    </div>
+		                    <div class='col-xs-6'>
+		                      <button id='" . $request_id . "' class='btn btn-sm width-60 btn-danger' value='" . $user_from . "' onclick='rejectRequest(this);deleteRequest(this);'> Reject</button>
+		                    </div>
+		                  </div>
+		                </div>
 			            </div>";
 				} elseif ($user_from == $user->getUsername()) {
 					$user_to_obj = new User($conn, $user_to);
-					$str .= "<div id='" . $row['request_id'] . "' class='p-3 col-12 request'>
-			              <div class='card' id='id'>
-			                <div class='card-body media'>
-				                <a href='profile.php?profile_username=". $user_to ."' class='text-primary'><img src='" . $user_to_obj->getProfilePic() . "' alt='profile-image' class='padding-5-circle' style='width:60px;height:60px;'/>
-			                  <div class='media-body pl-1'>
-			                    <h6>" . $user_to_obj->getFirstAndLastName() . "</h6></a><small class='text-muted'>
-			                    <em>" . $user->getMutualFriendsCount($user_to) . " mutual friends</em></small>
-			                  </div>
-			                  <div class='form-row' style='width:30%;'>
-			                    <div class='col-xs-6'>
-			                      <button id='" . $user_to . "' class='btn btn-warning btn-sm addfriend' onclick='friend(this)' value='2' onmouseleave='friendAction(this)'>Cancel Request</button>
-			                    </div>
-			                  </div>
-			                </div>
-			              </div>
+					$str .= "<div id='" . $row['request_id'] . "' class='p-3 m-3 col-12 request card'>
+		                <div class='card-body media'>
+			                <a href='profile.php?profile_username=". $user_to ."' class='text-primary'><img src='" . $user_to_obj->getProfilePic() . "' alt='profile-image' class='padding-5-circle' style='width:60px;height:60px;'/>
+		                  <div class='media-body pl-1'>
+		                    <h6>" . $user_to_obj->getFirstAndLastName() . "</h6></a><small class='text-muted'>
+		                    <em>" . $user->getMutualFriendsCount($user_to) . " mutual friends</em></small>
+		                  </div>
+		                  <div class='form-row' style='width:30%;'>
+		                    <div class='col-xs-6'>
+		                      <button id='" . $user_to . "' class='btn btn-warning btn-sm addfriend' onclick='friend(this)' value='2' onmouseleave='friendAction(this)'>Cancel Request</button>
+		                    </div>
+		                  </div>
+		                </div>
 			            </div>";
 				}
 			}
