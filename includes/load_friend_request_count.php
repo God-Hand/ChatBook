23c5aac18e9cb169bd3d551d1d7bb3fe040d0639
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-13 22:13:26 +0530

    registration is easy

--- includes/load_friend_request_count.php
+++ includes/load_friend_request_count.php
@@ -3,6 +3,7 @@
 	require 'classes/FriendRequest.php';
 	if(!isset($_SESSION['username'])){
 		echo "<script>window.location = 'index.php';</script>";
+		return 0;
 	}
 	$request = new FriendRequest($conn, $_SESSION['username']);
 	$count = $request->getFriendRequestsCount();
