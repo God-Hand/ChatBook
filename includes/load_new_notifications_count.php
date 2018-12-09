b3783fa42a2d5196adde6f64da689193f4708042
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-12-09 15:43:15 +0530

    update notice problem

--- includes/load_new_notifications_count.php
+++ includes/load_new_notifications_count.php
@@ -1,11 +1,15 @@
 <?php
-	require '../config/config.php';
-	require 'classes/Notification.php';
-	$notification = new Notification($conn, $_SESSION['username']);
-	$count = $notification->getUnreadNotificationsCount();
-	if($count > 99){
-		echo "99+";
-	}elseif($count != 0){
-		echo $count;
+	try{
+		require '../config/config.php';
+		require 'classes/Notification.php';
+		$notification = new Notification($conn, $_SESSION['username']);
+		$count = $notification->getUnreadNotificationsCount();
+		if($count > 99){
+			echo "99+";
+		}elseif($count != 0){
+			echo $count;
+		}	
+	}catch(Exception $e){
+		// do nothing on session out
 	}
 ?>
